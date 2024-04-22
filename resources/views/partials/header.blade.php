435fd0888122e749d219b65d9195a4228cd96918
danieledisco <daniele.disco.1965@gmail.com>
2024-04-22 11:14:30 +0200

    Inseriti filtraggi: con due pagine non funziona

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,7 +2,7 @@
     <header class="bg-dark">
         <nav class="nav justify-content-center  ">
             <a class="nav-link {{ Route::currentRouteName() === 'home' ? 'bg-primary text-white' : '' }} "
-                href="{{ route('home') }}" aria-current="page">Home</a>
+                href="{{ route('home') }}">Home</a>
             <a class="nav-link {{ Route::currentRouteName() === 'single' ? 'bg-primary text-white' : '' }}"
                 href="{{ route('single') }}">Single</a>
         </nav>
--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -5,13 +5,14 @@
         <h1>Movies List</h1>
         {{-- <div class="row row-cols-1 row-cols-md-3 g-3">
                 @foreach ($films as $film)
-                    <div class="card">
-                        <img class="card-img-top" width=422 height=632 src={{ $film->src }} alt=''>
-                        <div class="card-body">
-                            {{ $film->title }}
-                        </div>
-                    </div>
                 @endforeach
             </div> --}}
+        <div class="card">
+            <img class="card-img-top" src={{ $film->src }} alt=''>
+            <div class="card-body">
+                {{ $film->title }}
+            </div>
+        </div>
+
     </div>
 @endsection
